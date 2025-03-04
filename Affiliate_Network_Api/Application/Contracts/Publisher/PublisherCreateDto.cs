fc5a2db88ab988640c1f01482dbf768826185cfc
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-03-04 11:47:05 +0700

    Create Api for Publisher

---
+++ Affiliate_Network_Api/Application/Contracts/Publisher/PublisherCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Application.Contracts.Publisher
+{
+    public class PublisherCreateDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? CompanyName { get; set; }
+        public string? ContactName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? TaxId { get; set; }
+        public string? ReferredByCode { get; set; }
+        public int Role { get; set; } = 1; // Default role
+    }
+}
---
+++ Affiliate_Network_Api/Application/Mappings/PublisherMappingProfile.cs
@@ -0,0 +1,57 @@
+﻿using Application.Contracts.Publisher;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappings
+{
+    public class PublisherMappingProfile : Profile
+    {
+        public PublisherMappingProfile()
+        {
+            // Entity to DTO
+            CreateMap<Publisher, PublisherDto>()
+                .ForMember(dest => dest.TotalTrafficSources, opt =>
+                    opt.MapFrom(src => src.TrafficSources != null ? src.TrafficSources.Count : 0))
+                .ForMember(dest => dest.TotalCampaigns, opt =>
+                    opt.MapFrom(src => src.Promotes != null ? src.Promotes.Select(p => p.CampaignId).Distinct().Count() : 0));
+
+            // DTO to Entity
+            CreateMap<PublisherCreateDto, Publisher>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.RegistrationDate, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherId, opt => opt.Ignore())
+                .ForMember(dest => dest.ReferralCode, opt => opt.Ignore())
+                .ForMember(dest => dest.CampaignPublisherCommissions, opt => opt.Ignore())
+                .ForMember(dest => dest.FraudReports, opt => opt.Ignore())
+                .ForMember(dest => dest.PayoutRequests, opt => opt.Ignore())
+                .ForMember(dest => dest.Promotes, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherBalances, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherProfiles, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherReferralReferreds, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherReferralReferrers, opt => opt.Ignore())
+                .ForMember(dest => dest.TrafficSources, opt => opt.Ignore());
+
+            CreateMap<PublisherUpdateDto, Publisher>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+                .ForMember(dest => dest.RegistrationDate, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+                .ForMember(dest => dest.ReferralCode, opt => opt.Ignore())
+                .ForMember(dest => dest.ReferredByCode, opt => opt.Ignore())
+                .ForMember(dest => dest.CampaignPublisherCommissions, opt => opt.Ignore())
+                .ForMember(dest => dest.FraudReports, opt => opt.Ignore())
+                .ForMember(dest => dest.PayoutRequests, opt => opt.Ignore())
+                .ForMember(dest => dest.Promotes, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherBalances, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherProfiles, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherReferralReferreds, opt => opt.Ignore())
+                .ForMember(dest => dest.PublisherReferralReferrers, opt => opt.Ignore())
+                .ForMember(dest => dest.TrafficSources, opt => opt.Ignore());
+        }
+    }
+}
