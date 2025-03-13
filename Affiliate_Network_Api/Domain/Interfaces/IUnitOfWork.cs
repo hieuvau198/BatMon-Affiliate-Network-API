c32ad2f6accb267b8b4c597a9de632b085b4731f
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-03-14 02:28:42 +0700

    Update Service to use UoW

--- Affiliate_Network_Api/Domain/Interfaces/IUnitOfWork.cs
+++ Affiliate_Network_Api/Domain/Interfaces/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace Domain.Interfaces
 {
-    internal class IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        IGenericRepository<Admin> Admins { get; }
+        IGenericRepository<Advertiser> Advertisers { get; }
+        IGenericRepository<AdvertiserBalance> AdvertiserBalances { get; }
+        IGenericRepository<AdvertiserUrl> AdvertiserUrls { get; }
+        IGenericRepository<Campaign> Campaigns { get; }
+        IGenericRepository<CampaignAdvertiserUrl> CampaignAdvertiserUrls { get; }
+        IGenericRepository<CampaignConversionType> CampaignConversionTypes { get; }
+        IGenericRepository<CampaignPolicy> CampaignPolicies { get; }
+        IGenericRepository<CampaignPublisherCommission> CampaignPublisherCommissions { get; }
+        IGenericRepository<Conversion> Conversions { get; }
+        IGenericRepository<ConversionType> ConversionTypes { get; }
+        IGenericRepository<Currency> Currencies { get; }
+        IGenericRepository<DepositRequest> DepositRequests { get; }
+        IGenericRepository<FraudAdjustment> FraudAdjustments { get; }
+        IGenericRepository<FraudCase> FraudCases { get; }
+        IGenericRepository<FraudReport> FraudReports { get; }
+        IGenericRepository<FraudType> FraudTypes { get; }
+        IGenericRepository<Payment> Payments { get; }
+        IGenericRepository<PaymentMethod> PaymentMethods { get; }
+        IGenericRepository<PayoutRequest> PayoutRequests { get; }
+        IGenericRepository<PayoutRule> PayoutRules { get; }
+        IGenericRepository<Promote> Promotes { get; }
+        IGenericRepository<PromoteUrlvariant> PromoteUrlvariants { get; }
+        IGenericRepository<Publisher> Publishers { get; }
+        IGenericRepository<PublisherBalance> PublisherBalances { get; }
+        IGenericRepository<PublisherProfile> PublisherProfiles { get; }
+        IGenericRepository<PublisherReferral> PublisherReferrals { get; }
+        IGenericRepository<TrafficSource> TrafficSources { get; }
+        IGenericRepository<WithdrawalRequest> WithdrawalRequests { get; }
+
+        Task<int> SaveChangesAsync();
     }
 }
