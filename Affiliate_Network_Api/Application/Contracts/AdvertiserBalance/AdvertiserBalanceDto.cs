b97630b8671739303ad2339eb2f0818a7d6294e8
hoanvngoc <hoanvngoc2003@gmail.com>
2025-03-17 14:02:12 +0700

    Add Api for AdvertiserBalance

---
+++ Affiliate_Network_Api/Application/Contracts/AdvertiserBalance/AdvertiserBalanceDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts.AdvertiserBalance
+{
+    public class AdvertiserBalanceDto
+    {
+        public int BalanceId { get; set; }
+        public int? AdvertiserId { get; set; }
+        public decimal? AvailableBalance { get; set; }
+        public decimal? PendingBalance { get; set; }
+        public decimal? LifetimeDeposits { get; set; }
+        public decimal? LifetimeWithdrawals { get; set; }
+        public decimal? LifetimeSpend { get; set; }
+        public DateOnly? LastUpdated { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+
+    public class AdvertiserBalanceCreateDto
+    {
+        public int? AdvertiserId { get; set; }
+        public decimal? AvailableBalance { get; set; } = 0;
+        public decimal? PendingBalance { get; set; } = 0;
+        public string CurrencyCode { get; set; }
+    }
+
+    public class AdvertiserBalanceUpdateDto
+    {
+        public int BalanceId { get; set; }
+        public decimal? AvailableBalance { get; set; }
+        public decimal? PendingBalance { get; set; }
+        public decimal? LifetimeDeposits { get; set; }
+        public decimal? LifetimeWithdrawals { get; set; }
+        public decimal? LifetimeSpend { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+}
---
+++ Affiliate_Network_Api/Application/Mappings/AdvertiserBalanceProfile.cs
@@ -0,0 +1,18 @@
+﻿// AdvertiserBalanceProfile.cs
+using Application.Contracts.AdvertiserBalance;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappings
+{
+    public class AdvertiserBalanceProfile : Profile
+    {
+        public AdvertiserBalanceProfile()
+        {
+            CreateMap<AdvertiserBalance, AdvertiserBalanceDto>();
+            CreateMap<AdvertiserBalanceCreateDto, AdvertiserBalance>();
+            CreateMap<AdvertiserBalanceUpdateDto, AdvertiserBalance>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+        }
+    }
+}
